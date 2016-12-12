ca2043e1e700bdbe0593f758d823d49657724595
GeekGhc <ghcgavin@sina.com>
2016-12-12 11:55:03 +0800

    video

--- resources/views/video/play.blade.php
+++ resources/views/video/play.blade.php
@@ -16,6 +16,7 @@
                                class="video-js vjs-default-skin vjs-big-play-centered"
                                preload="auto"
                                tabindex="-1"
+                               controls
                                poster="/images/video/back.jpg"
                                data-setup='{"example_option":true}'
                         >
@@ -25,6 +26,24 @@
                                     video</a>
                             </p>
                         </video>
+                        <div class="video-catalog">
+                            <div style="color: #999;font-size: 18px">
+                                <a href="#">开发团队管理系统</a>
+                                <span style="font-weight: bold;"> &gt;&gt; </span>
+                                <a href="#">Laravel 开始使用VueJs</a>
+                                <span><i></i>2016-11-17</span>
+                                <div class="video-button-list">
+                                    <a class="btn btn-default"><i class="fa fa-star"></i>收藏</a>
+                                    <a class="btn btn-default"><i class="fa fa-download"></i>下载视频</a>
+                                    <a class="btn btn-default"><i class="fa fa-arrow-left"></i>上一节</a>
+                                    <a class="btn btn-default"><i class="fa fa-arrow-right"></i>下一节</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div style="color: #fff;font-size: 14px">
+                            Laravel结合vueJs实现实时评论
+                        </div>
+                        <div class="video-share"></div>
                     </div>
                     <!-- If you'd like to support IE8 -->
                     <script src="http://vjs.zencdn.net/ie8/1.1.2/videojs-ie8.min.js"></script>
@@ -35,35 +54,27 @@
     </div>
     <script>
         var options = {
-            controls: true,
-            techOrder : ["html5","flash"],
             "playbackRates":[0.5,1,1.25,1.5,1.75,2],
+            controls: true,
             bigPlayButton: true,
+            LoadingSpinner:true,
             textTrackDisplay: true,
             posterImage: false,
             errorDisplay: true,
             controlBar : {
+                volumeMenuButton: false,
+                currentTimeDisplay:false,
+                TimeDivider:true,
+                ProgressControl:false,
+                RemainingTimeDisplay:false,
+                /*DurationDisplay:true,
                 captionsButton : false,
                 chaptersButton: false,
                 subtitlesButton:false,
                 liveDisplay:false,
-                playbackRateMenuButton:false
+                playbackRateMenuButton:false*/
             }
-           /* controlBar: {
-                captionsButton: true,
-                CurrentTimeDisplay:true,
-                TimeDivider:true,
-                Duration:true,
-                chaptersButton: true,
-                subtitlesButton: true,
-                liveDisplay: true,
-                playbackRateMenuButton: true
-            }*/
-
         };
-        var player = videojs('ispace-video', options);
-        player.on('ready',function () {
-            console.log($(this));
-        })
+        var player = videojs('ispace-video', options)
     </script>
 @endsection
